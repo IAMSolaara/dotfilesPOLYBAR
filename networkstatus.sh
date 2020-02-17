7ba5d1431f97dc1074a04be74ec788f326bef0e5
lorecast162 <lorecastcauli@gmail.com>
2020-02-17 22:25:17 +0100

    added checks for iface info files

--- networkstatus.sh
+++ networkstatus.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 #wifi icons
 ICON_WIFI_DISCONNECTED='睊'
 ICON_WIFI_CONNECTED='直'
@@ -7,23 +9,31 @@ ICON_ETHERNET_DISCONNECTED=''
 ICON_ETHERNET_CONNECTED=''
 
 wifi() {
-	local IFACE=`cat ./wifi`
-	STATEFILE='/sys/class/net/'$IFACE'/operstate'
+	if [[ -f ./wifi ]]; then
+		local IFACE=`cat ./wifi`
+		STATEFILE='/sys/class/net/'$IFACE'/operstate'
 
-	case `cat $STATEFILE` in
-		up)	echo "%{F#11FF11}"$ICON_WIFI_CONNECTED": "`ip -4 addr show $IFACE | grep -oP '(?<=inet\s)\d+(\.\d+){3}'`' ('`iwgetid --raw`')' ;;
-		down) echo "%{F#FF1111}"$ICON_WIFI_DISCONNECTED": DOWN" ;;
-	esac
+		case `cat $STATEFILE` in
+			up)	echo "%{F#11FF11}"$ICON_WIFI_CONNECTED": "`ip -4 addr show $IFACE | grep -oP '(?<=inet\s)\d+(\.\d+){3}'`' ('`iwgetid --raw`')' ;;
+			down) echo "%{F#FF1111}"$ICON_WIFI_DISCONNECTED": DOWN" ;;
+		esac
+	else
+		echo "Please add interface info file.";
+	fi
 }
 
 ethernet() {
-	local IFACE=`cat ./ethernet`
-	STATEFILE='/sys/class/net/'$IFACE'/operstate'
+	if [[ -f ./ethernet ]]; then
+		local IFACE=`cat ./ethernet`
+		STATEFILE='/sys/class/net/'$IFACE'/operstate'
 
-	case `cat $STATEFILE` in
-		up)	echo "%{F#11FF11}"$ICON_ETHERNET_CONNECTED": "`ip -4 addr show $IFACE | grep -oP '(?<=inet\s)\d+(\.\d+){3}'` ;;
-		down) echo "%{F#FF1111}"$ICON_ETHERNET_DISCONNECTED": DOWN" ;;
-	esac
+		case `cat $STATEFILE` in
+			up)	echo "%{F#11FF11}"$ICON_ETHERNET_CONNECTED": "`ip -4 addr show $IFACE | grep -oP '(?<=inet\s)\d+(\.\d+){3}'` ;;
+			down) echo "%{F#FF1111}"$ICON_ETHERNET_DISCONNECTED": DOWN" ;;
+		esac
+	else
+		echo "Please add interface info file.";
+	fi
 }
 
 case $1 in
@@ -34,11 +44,11 @@ esac
 
 #from polybar config
 #label-disconnected-foreground = #FF1111
-#label-disconnected = : DOWN
+#label-disconnected = : DOWN
 #label-connected-foreground = #11FF11
-#label-connected = : %local_ip%
-#
+#label-connected = : %local_ip%
+
 #label-disconnected-foreground = #FF1111
-#label-disconnected = 
+#label-disconnected = 睊
 #label-connected-foreground = #11FF11
-#label-connected = : %local_ip% (%essid%)
+#label-connected = 直: %local_ip% (%essid%)
