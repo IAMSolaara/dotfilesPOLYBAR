d81aacead08d096eaf89aed9dd8dc452b6c25748
lorecast162 <lorecastcauli@gmail.com>
2020-02-17 21:23:05 +0100

    still doin it

--- polybar.sh
+++ polybar.sh
@@ -10,10 +10,17 @@ while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 export DEFAULT_ETHERNET=$(ip route | grep '^default' | awk '{print $5}' | grep e)
 export DEFAULT_WIFI=$(ip route | grep '^default' | awk '{print $5}' | grep w)
 
+#excluding this until there is a way to exclude mounts in polybar
+#MCONT=0
+#
+#for x in `df | awk '/^\/dev\//{print $6}' | grep -v 'boot'`; do
+#	CURMOUNT="POLYBAR_MOUNT"$MCONT
+#	export $CURMOUNT=$x
+#	let "MCONT+=1"
+#done
+
 #launch polybar with default config
-#polybar vgabar1 &
 polybar top &
 polybar bottom &
-
 
 echo "polybar running..."
